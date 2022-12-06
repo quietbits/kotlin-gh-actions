50f6510d484860267488eb57bbadc3463f59774a
Iveta <quietbits@gmail.com>
2022-12-06 14:25:27 -0500

    Basic project setup with tests

---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class MainKtTest {
+    @Test
+    fun `add two numbers correctly`() {
+        kotlin.test.assertEquals(3, addTwoNumbers(1, 2))
+    }
+
+    @Test
+    fun `add two numbers test`() {
+        kotlin.test.assertEquals(10, addTwoNumbers(1, 2))
+    }
+
+    @Test
+    fun `add two strings correctly`() {
+        kotlin.test.assertEquals("ab", addTwoStrings("a", "b"))
+    }
+
+    @Test
+    fun `add two strings test`() {
+        kotlin.test.assertEquals("3", addTwoStrings("1", "2"))
+    }
+}
